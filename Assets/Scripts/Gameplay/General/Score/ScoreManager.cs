e8021bc787023900fa0e33bdf1598f5e77a9c5aa
Sodya <damirmavlyutov@syndicate-g.com>
2025-10-08 17:54:37 +0400

    changes ui, code

--- Assets/Scripts/Gameplay/General/Score/ScoreManager.cs
+++ Assets/Scripts/Gameplay/General/Score/ScoreManager.cs
@@ -55,7 +55,7 @@ namespace Gameplay.General.Score
         {
             TotalScoreReactive.Value += Mathf.RoundToInt(value * _multiplier.Current);
             
-            _currencyManager.Add(1);
+            _currencyManager.Add(CurrencyType.Gold, 1);
         }
 
         private void ResetScore()
